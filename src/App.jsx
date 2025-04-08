c7b05631e5e4402ec0f431e854e3227e4b951834
KirtiJha <kirti.jha2025@gmail.com>
2025-04-08 11:34:24 +0530

    added some function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
 import styled from "styled-components";
 import StartGame from "./components/StartGame";
+import { useState } from 'react';
 
 // const Button = styled.button `background-color: black; color: white; padding: 10px`;
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
 
   return (
     <>
-     {/*  <Button>This is Styled Component Button</Button> */}
-     <StartGame/>
+      {/*  <Button>This is Styled Component Button</Button> */}
+      <StartGame />
     </>
   );
 }
--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -9,7 +9,7 @@ export default function StartGame ()  {
       </div>
       <div className="content">
         <h1>Dice Game</h1>
-        <button>Play Now</button>
+        <Button>Play Now</Button>
       </div>
     </Container>
   );
@@ -17,5 +17,38 @@ export default function StartGame ()  {
 
 const Container = styled.div`
     max-width: 1180px;
-`
+    height: 100vh;
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
 
+    .content { /* ye saas type css ko likhe hai*/
+      h1{
+        font-size: 96px;
+        white-space: nowrap;   /* isse sara h1 ka text ek hi line me hoga*/
+      }
+    }
+`;
+
+const Button = styled.button`
+    color: white;
+    padding: 10px 18px;
+
+    width: 220px;
+
+    background: #000000;
+    border-radius: 5px;
+
+    border: none;
+    font-size: 16px;
+    border: 1px solid transparent;
+    transition: 0.4s background ease-in;
+    cursor: pointer;
+
+    &:hover {  /* here, & is sudo for hover effect on btn*/
+    background-color: white;
+    border: 1px solid black;
+    color: black;
+    transition: 0.3s background ease-in;
+  }
+`;
