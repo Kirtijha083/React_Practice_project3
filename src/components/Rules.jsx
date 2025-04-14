f2f28269a91158d935399f112dbb6a87fab4568a
KirtiJha <kirti.jha2025@gmail.com>
2025-04-14 12:29:00 +0530

    add rules component

---
+++ src/components/Rules.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function Rules(){
+    return(
+        <RulesContainer>
+        <h2>How to play dice game</h2>
+        <div className="text">
+          
+        </div>
+      </RulesContainer>
+    );
+}
